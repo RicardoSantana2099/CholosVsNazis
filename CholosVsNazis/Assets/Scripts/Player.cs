9bb794dee69844c81ab44b94d9b4e9732b4eed01
PACHECO 2099 <pacheco2099.working@gmail.com>
2023-08-07 21:08:34 -0600

    Actualización 0.6
    El jugador ahora recibe daño de balas y tocando a enemigos

--- CholosVsNazis/Assets/Scripts/Player.cs
+++ CholosVsNazis/Assets/Scripts/Player.cs
@@ -9,21 +9,19 @@ public class Player : MonoBehaviour
     private float Horizontal;
     private bool Grounded;
     private int Health = 3;
-   
+    private bool isInvulnerable = false; // Variable de control de invulnerabilidad
+    public float invulnerableDuration = 1.0f; // Duración de la invulnerabilidad en segundos
 
     public GameObject BulletPrefab;
     public float Jumpforce;
     public float Speed;
-    
-    
+
     void Start()
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        
     }
 
-    
     void Update()
     {
         Horizontal = Input.GetAxisRaw("Horizontal");
@@ -31,7 +29,7 @@ public class Player : MonoBehaviour
 
         if (Horizontal < 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
         else if (Horizontal > 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-        
+
         Debug.DrawRay(transform.position, Vector3.down * 2f, Color.red);
         if (Physics2D.Raycast(transform.position, Vector3.down, 2f))
         {
@@ -39,18 +37,15 @@ public class Player : MonoBehaviour
         }
         else Grounded = false;
 
-        if(Input.GetKeyDown(KeyCode.W) && Grounded)
+        if (Input.GetKeyDown(KeyCode.W) && Grounded)
         {
             Jump();
         }
-
-       
     }
 
     private void FixedUpdate()
     {
         Rigidbody2D.velocity = new Vector2(Horizontal * Speed, Rigidbody2D.velocity.y);
-
     }
 
     private void Jump()
@@ -60,7 +55,42 @@ public class Player : MonoBehaviour
 
     public void Hit()
     {
-        Health = Health - 1;
-        if (Health == 0) Destroy(gameObject); 
+        if (!isInvulnerable) // Verificar si el jugador es vulnerable
+        {
+            Health -= 1;
+            Debug.Log("Recibió daño. Vidas restantes: " + Health);
+
+            if (Health <= 0)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                StartCoroutine(InvulnerabilityTimer()); // Activar la invulnerabilidad temporal
+            }
+        }
+    }
+
+    private IEnumerator InvulnerabilityTimer()
+    {
+        isInvulnerable = true; // Hacer que el jugador sea invulnerable
+        yield return new WaitForSeconds(invulnerableDuration); // Esperar por la duración de invulnerabilidad
+        isInvulnerable = false; // Desactivar la invulnerabilidad
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Bala"))
+        {
+            Hit();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemigo"))
+        {
+            Hit();
+        }
     }
 }
